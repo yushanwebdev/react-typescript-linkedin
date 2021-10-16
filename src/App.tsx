e0e63d53fa7496962ef193b98c19e0da9cdfcca4
yushan <yushan@intervest.lk>
2021-10-16 15:34:21 +0530

    feat: Develop the <Message />

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,14 @@
 import logo from './logo.svg';
+import Message from './Message';
+
 import './App.css';
 
 function App() {
-  // tuple
-  let aTuple: [string, number] = ["Yushan", 2021];
-  // enum -> To store magical values
-  enum Codes {
-    first = 1,
-    second
-  }
-  // any
-  let firstName: any = "yushan";
-  // void
-  const warning = (): void => {
-    console.log("Warning");
-  }
-
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          The value {firstName} is of {typeof firstName} type!
-        </p>
+        <Message />
       </header>
     </div>
   );
---
+++ src/Message.tsx
@@ -0,0 +1,7 @@
+const Message = () => {
+    return (
+        <p>This is a quick message!!!</p>
+    )
+}
+
+export default Message;
