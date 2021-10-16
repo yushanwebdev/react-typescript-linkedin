c6429e3a09ac86916eb31decee481752c34ec9f1
yushan <yushan@intervest.lk>
2021-10-16 13:58:37 +0530

    feat: Typescript basic types added

--- src/App.tsx
+++ src/App.tsx
@@ -2,21 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let firstValue: string = "Yushan";
+  let secondValue: number = 10;
+  let thirdValue: boolean = true;
+  // array
+  let fourthValue: number[] = [1, 3, 5];
+  let fifthValue: Array<string> = ['2', '5', 'Yushan'];
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          The value {fifthValue} is of {typeof fifthValue} type!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
